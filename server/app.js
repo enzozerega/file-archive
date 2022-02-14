2e7be5c3b01bc2369ef69815c48114be9cdfd829
Enzo Zerega <enzo.zerega@chasacademy.se>
2022-02-14 18:30:33 +0100

    Remove unused code

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const home = require("./routes/index");
 const fileRoutes = require("./routes/files");
 const userRoutes = require("./routes/users");
 
@@ -10,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/", home);
 app.use("/api/files", fileRoutes);
 app.use("/api/users", userRoutes);
 
